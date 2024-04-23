275c42337296a46fa715bc4a9f99ed63502de6a9
Hetal Mehta <hetalmehta@vebuin.com>
2024-04-23 11:20:07 +0530

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Hetal Mehta on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
